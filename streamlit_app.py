03c27a04a1b5d8a13b5dadce6fe4b37d5aa4bc0c
132thlig <yunaviva12@gmail.com>
2024-11-05 10:52:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My toto app")
+st.title("🎈 My toto's app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
